83e23bae0f5c1237da5b62d7901b9fbc872130a7
JASPER <jasjasper101@gmail.com>
2022-07-29 17:14:33 -0700

    refactor: Able to change the the query build within postgres for getProductInfo

--- PostgreSQL/models.js
+++ PostgreSQL/models.js
@@ -11,9 +11,23 @@ var getAllProducts = async (count, page) => {
 };
 
 var getProductInfo = async (id) => {
-  var results = await client.query(`SELECT json_agg(name) FROM product_features group by id where id = ${id} limit 5`)
-
-  return results.rows
+  var results = await client.query(`SELECT json_build_object(
+                                      'id', ${id},
+                                      'name', p.name,
+                                      'slogan', p.slogan,
+                                      'description', p.description,
+                                      'category', p.category,
+                                      'default_price', p.default_price,
+                                      'features', json_agg(json_build_object(
+                                        'feature', r.feature,
+                                        'value', r.value
+                                        ))
+                                      )
+                                      FROM products p
+                                      LEFT JOIN product_features r ON p.id = r.product_id
+                                      WHERE p.id = ${id}
+                                      GROUP BY p.name, p.slogan, p.description, p.category, p.default_price`)
+  return results.rows[0].json_build_object;
   client.end();
 };
 
@@ -22,7 +36,9 @@ var getProductStyles = async (id) => {
 };
 
 var getRelatedProducts = async(id) => {
-  var relatedProducts = await client.query(`SELECT json_agg(related_product_id) FROM related_products WHERE product_id = ${id}`);
+  var relatedProducts = await client.query(`SELECT json_agg(related_product_id)
+                                            FROM related_products
+                                            WHERE product_id = ${id}`);
   return relatedProducts.rows[0].json_agg;
   client.end();
 };
@@ -35,19 +51,32 @@ module.exports.getRelatedProducts = getRelatedProducts;
 
 
 
-// var getProductInfo = async (id) => {
-//   var results = await client.query(`SELECT json_build_object(
-//                                               'id', ${id},
-//                                               'name', p.name,
-//                                               'slogan', p.slogan,
-//                                               'description', p.description,
-//                                               'category', p.category,
-//                                               'default_price', p.default_price
-//                                             )
-//                                             FROM products p
-//                                             WHERE p.id = ${id}
-//                                          `)
 
-//   return results.rows[0].json_build_object;
+// var getProductInfo = async (id) => {
+//   var results = await client.query(`SELECT json_build_object('feature', json_agg(json_build_object('feature', p.feature, 'value', p.value))) FROM product_features p where product_id = ${id} limit 5`)
+
+//   return results.rows
+//   client.end();
+// };
+
+
+
+
+// var getProductInfo = async (id) => {
+//   var product = await client.query(`SELECT * FROM products WHERE id = ${id}`);
+//   var productFeatures = await client.query(`SELECT json_build_object(
+//                                               'feature', json_agg(
+//                                                 json_build_object(
+//                                                   'feature', p.feature,
+//                                                    'value', p.value
+//                                                    )
+//                                                   )
+//                                                 )
+//                                             FROM product_features p
+//                                             WHERE product_id = ${id}`)
+
+//   var results = product.rows[0];
+//   results.features = productFeatures.rows[0].json_build_object.feature
+//   return results
 //   client.end();
 // };
--- PostgreSQL/routesSQL.js
+++ PostgreSQL/routesSQL.js
@@ -11,11 +11,8 @@ routerSQL.get('/products', (request, response) => {
 
   var getAllProducts = async () => {
     var results = await models.getAllProducts(count, page);
-    try {
-      response.status(200).send(results)
-    } catch(error) {
-      console.log('There is an error in retrieving all of the products')
-    }
+    try { response.status(200).send(results) }
+    catch(error) { console.log('There is an error in retrieving all of the products') }
   };
   getAllProducts();
 });
@@ -24,11 +21,8 @@ routerSQL.get('/products', (request, response) => {
 routerSQL.get('/product/:id', (request, response) => {
   var getProductInfo = async () => {
     var results = await models.getProductInfo(request.params.id);
-    try {
-      response.status(200).send(results)
-    } catch(error) {
-      console.log('Error in getting product info from server side')
-    }
+    try { response.status(200).send(results) }
+    catch(error) { console.log('Error in getting product info from server side') }
   };
   getProductInfo();
 });
@@ -36,27 +30,19 @@ routerSQL.get('/product/:id', (request, response) => {
 routerSQL.get('/products/:product_id/related', (request, response) => {
   var getRelatedProducts = async () => {
     var results = await models.getRelatedProducts(request.params.product_id)
-    try {
-      response.status(200).send(results)
-    } catch (error) {
-      console.log('Error in retrieving related products from server side')
-    }
+    try { response.status(200).send(results) }
+    catch (error) { console.log('Error in retrieving related products from server side')}
   };
-  getRelatedProducts()
+  getRelatedProducts();
 });
 
-// routerSQL.get('/:id/styles', (request, response) => {
-//   var getProductStyles = async () => {
-//     var results = await models.getProductStyles(request.params.id);
-
-//     try {
-//       response.status(200).send(results)
-//     } catch(error) {
-//       console.log('Error in getting product styles from server side')
-//     }
-//   };
-
-//   getProductStyles();
-// });
+routerSQL.get('/:id/styles', (request, response) => {
+  var getProductStyles = async () => {
+    var results = await models.getProductStyles(request.params.id);
+    try { response.status(200).send(results) }
+    catch(error) { console.log('Error in getting product styles from server side') }
+  };
+  getProductStyles();
+});
 
 module.exports = routerSQL
